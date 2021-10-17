b58fd21ad8004fd98fcfc00b057e2b62a1e076c9
hezron <hezrongimeno85@gmail.com>
2021-10-17 09:32:46 +0300

    created a spring boot project

---
+++ src/test/java/tech/hackathon/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hackathon.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
